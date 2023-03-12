73f77a0c85574c77366fa8191d5f5fe63387e2bc
JuliaTomchuk <yuliamalykowa@gmail.com>
2023-03-12 13:37:43 +0300

    13 37

---
+++ lesson_42_spring-boot/src/main/java/com/example/lesson_42_springboot/Lesson42SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.lesson_42_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson42SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson42SpringBootApplication.class, args);
+    }
+
+}
